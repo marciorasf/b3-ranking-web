31b40fa6315699457ea75bc3e8f868f2c3672d76
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-16 22:53:50 -0300

    Add table with imported stocks.

--- pages/last-import.tsx
+++ pages/last-import.tsx
@@ -1,19 +1,49 @@
 import type { NextPage } from 'next'
-import Link from "next/link"
 
-import { Link as MuiLink, Grid, Typography, Button } from "@material-ui/core"
-import { useState } from "react"
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Grid,
+  Popover,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@material-ui/core"
+import { Visibility as ShowIndicatorsIcon } from "@material-ui/icons"
+import { ReactNode, useState } from "react"
 
 import serverApi from '../utils/server-api'
 import { StockImport } from '../utils/protocols'
 import useDidMount from '../hooks/useDidMount'
+import { Spacing } from '../components'
 
 const LastImport: NextPage = () => {
   const [lastImport, setLastImport] = useState<StockImport>()
+  const [popoverData, setPopoverData] = useState<{
+    element: Element,
+    stock: Stock
+  } | null>(null)
 
   async function getAndUpdateLastImport() {
     const result = await serverApi.lastImport()
+    console.log(result)
     setLastImport(result)
+  }
+
+  function handleOpenPopover(target: EventTarget, stock: Stock) {
+    setPopoverData({
+      element: target,
+      stock: stock
+    })
+  }
+
+  function handleClosePopover() {
+    setPopoverData(null)
   }
 
   useDidMount(() => {
@@ -21,33 +51,111 @@ const LastImport: NextPage = () => {
   })
 
   return lastImport ? (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Link href="/" passHref>
-          <MuiLink>
-            Home
-          </MuiLink>
-        </Link>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Button onClick={serverApi.forceImport} color="primary" variant="contained">
+            Force New Import
+          </Button>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Box display="flex" alignItems="center">
+            <Typography variant="h6">
+              Date:
+            </Typography>
+
+            <Spacing orientation="vertical" size={1} />
+
+            <Typography variant="body1">
+              {new Date(lastImport.date).toLocaleString("en-uk", { dateStyle: "short", hour12: false, timeStyle: "long" })}
+            </Typography>
+          </Box>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Box display="flex" alignItems="center">
+            <Typography variant="h6">
+              Errors:
+            </Typography>
+
+            <Spacing orientation="vertical" size={1} />
+
+            <Typography variant="body1">
+              {lastImport.importErrors.length > 0 ? lastImport.importErrors : "None"}
+            </Typography>
+          </Box>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  Stock Code
+                </TableCell>
+
+                <TableCell>
+                  Average Daily Liquidity
+                </TableCell>
+
+                <TableCell>
+                  Show Indicators
+                </TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {lastImport.stocks.map(stock => (
+                <TableRow>
+                  <TableCell>
+                    {stock.code}
+                  </TableCell>
+
+                  <TableCell>
+                    {stock.indicatorsValues.liquidez_media_diaria
+                      ? `R$ ${Math.round(stock.indicatorsValues.liquidez_media_diaria)}`
+                      : "-"
+                    }
+                  </TableCell>
+
+                  <TableCell>
+                    <IconButton onClick={(event) => handleOpenPopover(event.currentTarget, stock)}>
+                      <ShowIndicatorsIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Grid>
       </Grid>
 
-      <Grid item xs={12}>
-        <Button onClick={serverApi.forceImport} color="primary" variant="contained">
-          Force Last Import
-        </Button>
-      </Grid>
-
-      <Grid item xs={12}>
-        Date: {new Date(lastImport.date).toLocaleString("en-uk", { dateStyle: "short", hour12: false, timeStyle: "long" })}
-      </Grid>
-
-      <Grid item xs={12}>
-        Errors: {lastImport.importErrors.length > 0 ? lastImport.importErrors : "None"}
-      </Grid>
-
-      <Grid item xs={12}>
-        Stocks: {lastImport.stocks.map(stock => stock.code).join(", ")}
-      </Grid>
-    </Grid>
+      <Popover
+        open={Boolean(popoverData)}
+        anchorEl={popoverData?.element}
+        onClose={handleClosePopover}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+      >
+        {popoverData?.stock && (
+          <Container>
+            <Grid container spacing={1}>
+              {Object.entries(popoverData?.stock.indicatorsValues).map(([indicator, value]) => (
+                <Grid item xs={12}>
+                  <Typography variant="body2">
+                    {indicator}: {value}
+                  </Typography>
+                </Grid>
+              )
+              )}
+            </Grid>
+          </Container>
+        )}
+      </Popover>
+    </>
   ) : (
     <Typography variant="h4">
       Fetching data...
