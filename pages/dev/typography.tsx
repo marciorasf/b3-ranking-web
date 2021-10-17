af65e1269ef98acc782a4af9f782ee93b51e73c6
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-16 22:20:16 -0300

    Add typography and palette pages.

---
+++ pages/dev/typography.tsx
@@ -0,0 +1,110 @@
+import React from "react"
+
+import { Typography, Grid } from "@material-ui/core"
+
+const TypographyDemo = () => {
+  return (
+    <Grid container direction="column" spacing={2}>
+      <Grid item>
+        <Typography variant="h1">
+          h1: Cupidatat nulla culpa do esse.
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="h2">
+          h2: Cupidatat nulla culpa do esseSit dolore consectetur fugiat
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="h3">
+          h3: Cupidatat nulla culpa do esseUt sit commodo irure consequat
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="h4">
+          h4: Cupidatat nulla culpa do esseAnim dolor reprehenderit labore
+          incididunt esse nostrud labore incididunt adipisicing
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="h5">
+          h5: Cupidatat nulla culpa do esseLabore incididunt commodo
+          cillum culpa voluptate minim cillum mollit id.Ullamco consequat
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="h6">
+          h6: Cupidatat nulla culpa do esseIrure excepteur proident
+          exercitation fugiat sint voluptate consequat adipisicing non
+          deserunt..
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="subtitle1">
+          subtitle1: Cupidatat nulla culpa do esseQui amet elit ipsum non
+          ut fugiat officia.Laborum enim et ex irure Lorem sunt pariatur
+          est mollit incididunt..
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="subtitle2">
+          subtitle2: Cupidatat nulla culpa do esseUllamco officia ad et
+          qui nisi voluptate laborum id ex nulla nisi sint sunt
+          ullamco.Laboris irure quis elit id do incididunt proident
+          eiusmod elit reprehenderit elit fugiat eu..
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="body1">
+          body1: Cupidatat nulla culpa do esseLaborum dolor cupidatat
+          nostrud aliquip reprehenderit pariatur tempor dolor elit
+          ea.Exercitation Lorem laboris voluptate excepteur esse ut
+          deserunt occaecat officia mollit do elit aute quis..
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Typography variant="body2">
+          body2: Cupidatat nulla culpa do esseCillum aliquip ea ullamco id
+          labore fugiat esse.Excepteur fugiat quis cillum mollit laborum
+          ipsum deserunt amet proident enim Lorem enim id nisi..
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="button">
+          button: Cupidatat nulla culpa do esseEa quis et enim veniam
+          aute.Aute deserunt sunt Lorem eiusmod dolore in.Labore nisi
+          dolor ea do ullamco amet..
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="caption">
+          caption: Cupidatat nulla culpa do essDolore anim sint
+          reprehenderSunt cillum amet proident Lorem.Et veniam veniam
+          deserunt ut qui.it iLorem officia sit magna ea incididunt
+          pariatur consectetur cillum Lorem reprehenderit enim culpa qui
+          officia.psum nostrud.e.
+        </Typography>
+      </Grid>
+
+      <Grid item>
+        <Typography variant="overline">
+          overline: Cupidatat nulla culpa do esEnim id consequat irure
+          nostrud Lorem incididunt labore do ullamco culpa eu minim ex
+          adipisicing.Ipsum ipsum cillum enim sint.se.
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default TypographyDemo
